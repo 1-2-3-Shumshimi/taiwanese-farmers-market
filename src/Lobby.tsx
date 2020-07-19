8a87ff4b24ea79adf67b92a114227600e76f9164
jeffrey-gu <jeffreygu@wustl.edu>
2020-07-19 16:39:05 -0700

    Start adding multiplayer and lobby inputs

---
+++ src/Lobby.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Lobby } from 'boardgame.io/react';
+
+<Lobby
+    gameServer = {`https://${window.location.hostname}:8000`}
+    lobbyServer = {`https://${window.location.hostname}:8000`}
+    // gameComponents = {importedGames}
+/>;
