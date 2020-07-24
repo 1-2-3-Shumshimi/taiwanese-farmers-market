2560dcc6a6928d36571bceaac2076058f7be5a29
jeffrey-gu <jeffreygu@wustl.edu>
2020-07-23 22:08:06 -0700

    Test Local() multiplayer option in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,23 @@ import { Local } from 'boardgame.io/multiplayer';
 // import { SocketIO } from 'boardgame.io/multiplayer';
 import { TFM } from './Game';
 import { Board } from './Board';
+import { TransportOpts } from 'boardgame.io/dist/types/src/client/transport/transport';
 
 const TFMClient = Client({
   game: TFM,
   board: Board,
-  multiplayer: Local(),
-  // multiplayer: SocketIO({ server: 'localhost:3000' }),
+  debug: false,
+  multiplayer: (opts: TransportOpts) => {
+    return Local()({...opts, gameKey: TFM, game: TFM as any});
+  },
+  // multiplayer: SocketIO({ server: 'localhost:8000' }),
 });
 
 const App = () => (
     <div>
         <TFMClient playerID="0" />
         <TFMClient playerID="1" />
+        <TFMClient playerID="2" />
     </div>
 );
 
--- src/Server.ts
+++ src/Server.ts
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Server } from 'boardgame.io/server';
+import { Server, FlatFile } from 'boardgame.io/server';
 import { TFM } from "./Game";
-import { SERVER_PORT } from "./utils/Constants"
+// import { LobbyConfig } from './Lobby'
+// import Router from 'koa-router';
+// import { SERVER_PORT } from "./utils/Constants"
+
+// const router = new Router();
 
 const server = Server({
     games: [TFM],
+    db: new FlatFile({
+        dir: '/storage/directory',
+        logging: true,
+    }),
 });
 
-server.run(SERVER_PORT);
+// server.run({ port: SERVER_PORT, LobbyConfig, () => console.log("server running") });
+server.run(8000, () => console.log("Server running"));
